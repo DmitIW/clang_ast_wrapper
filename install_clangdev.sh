4a1d7e1552cf9816b0a635b80a5eda6c8d4f3b95
d.ivakhnenko <d.ivakhnenko@ddos-guard.net>
2020-11-06 13:25:29 +0300

    Update installing clang-dev script

--- install_clangdev.sh
+++ install_clangdev.sh
@@ -1,4 +1,4 @@
 sudo apt-get update;
 sudo apt-get install -y libclang-dev;
-pip install -r ./clangast/requirements.txt;
-dpkg -L $(dpkg --get-selections | grep libclang-) | grep libclang.so > clangast/path_to_so.txt
+pip install -r ./requirements.txt;
+dpkg -L $(dpkg --get-selections | grep libclang-) | grep libclang.so
